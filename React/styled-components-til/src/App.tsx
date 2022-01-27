6c78101b5f258abce070f96ce7c1eaf10ba68ea8
artinc5233 <artinc5233@gmail.com>
2022-01-27 17:15:25 +0900

    css - typescrip를 이용해 styped-components theme 적용하기

--- React/styled-components-til/src/App.tsx
+++ React/styled-components-til/src/App.tsx
@@ -1,12 +1,22 @@
+import styled from 'styled-components';
 import { Circel } from './Circel';
 
 function App() {
   return (
-    <>
+    <BackgoundColor>
       <Circel bgColor="teal" />
       <Circel bgColor="tomato" boderColor="teal" />
-    </>
+      <span>Theme Color</span>
+    </BackgoundColor>
   );
 }
+
+const BackgoundColor = styled.main`
+  background-color: ${props => props.theme.backgroundColor};
+  color: ${props => props.theme.Color};
+  span {
+    font-size: 5rem;
+  }
+`;
 
 export default App;
---
+++ React/styled-components-til/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const darkTheme: DefaultTheme = {
+  Color: 'white',
+  backgroundColor: '#111',
+};
+
+const whiteTheme: DefaultTheme = {
+  Color: '#111',
+  backgroundColor: 'white',
+};
+
+export { darkTheme, whiteTheme };
